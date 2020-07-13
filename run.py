8e57f17c83d5aa17e96c509798f4da2e26542574
Amirreza A <arjmandamirreza@gmail.com>
2020-07-13 22:38:18 +0430

    fixed minor bugs

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
 import main
 # import main_counter as main
 
-lf = main_counter.LetterFrequency()   # You can also sprcify the name of the image in this class
+lf = main.LetterFrequency("fig.png")   # You can also sprcify the name of the image in this class
                                       # if you want to save the data in the format of an image
 
 lf.count_letters()
